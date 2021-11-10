c9b625012ac2d4fad866a3784e98821fcb7fe337
prybula_roman <roman.bismail@gmail.com>
2021-11-10 16:53:59 +0200

    10.11.2021

--- dist/index.379dd93c.js
+++ dist/index.379dd93c.js
@@ -459,18 +459,39 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"hD4hw":[function(require,module,exports) {
-var debounce = require("lodash.debounce");
-//console.log(debounce);
-fetch("https://jsonplaceholder.typicode.com/users").then((response)=>{
-    if (!response.ok) throw new Error(response.status);
-    console.log(response.json());
-    return response.json();
-}).then((data)=>{
-    // Data handling
-    console.log(data);
-}).catch((error)=>{
-// Error handling
-});
+const debounce = require("lodash.debounce");
+console.dir(debounce);
+let i = 0;
+const arr = new Array();
+const searchCountry = (e)=>{
+    console.log(e.target.value);
+};
+const setCountry = document.querySelector(".country");
+//console.log(cntry);
+setCountry.addEventListener("input", debounce((e)=>{
+    i = 0;
+    //console.log(e.target.value);
+    fetch("https://restcountries.com/v2/all").then((response)=>{
+        if (!response.ok) throw new Error(response.status);
+        // console.log(response.json());
+        return response.json();
+    }).then((data)=>{
+        // Data handling
+        // console.log(data);
+        data.forEach((element)=>{
+            i++;
+            // console.log(`${i} name = `, element.name);
+            if (element.name.indexOf(e.target.value) != -1) arr.push(element.name);
+        });
+        console.log(arr.length);
+        arr.forEach((elem)=>console.log(`arr${[
+                i
+            ]} =${elem} `)
+        );
+    }).catch((error)=>{
+    //   console.log(Error.Error);
+    });
+}, 500));
 
 },{"lodash.debounce":"dYpAa"}],"dYpAa":[function(require,module,exports) {
 var global = arguments[3];
