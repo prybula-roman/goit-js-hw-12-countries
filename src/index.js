e868c243938f34e8bbb5d3858ae0d3c93223c33b
prybula_roman <roman.bismail@gmail.com>
2021-12-01 16:53:28 +0200

    1.12.2021

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ console.log(debounce);
 
 let i = 0;
 //const arr = new Array();
+//====================
+const countries = {
+  name: "",
+};
+//=======================
+
+let names = new Array();
 
 const inp = document.querySelector(".country");
 console.log(inp);
@@ -15,33 +22,39 @@ setCountry.addEventListener(
   "input",
   debounce((e) => {
     i = 0;
-    console.log("---------", e.target.value);
+    console.log("---------e.target.value=", e.target.value);
     fetch("https://restcountries.com/v2/all")
       .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
+        // if (!response.ok) {
+        //   throw new Error(response.status);
+        // }
         //  console.log(response.json());
         return response.json();
       })
       .then((resp) => {
-        console.log("~~~~~~~~~", resp);
-        console.log("~~~~~~~~~", e.target.value);
+        console.log("~~~~~~~~~resp=", resp);
+        console.log("~~~~~~~~~e.target.value=", e.target.value);
         // console.log(data);
         resp.forEach((el) => {
           if (
             el.name.toLowerCase().indexOf(e.target.value.toLowerCase()) != -1
           ) {
             console.log("el.name=", el.name);
-            inp.insertAdjacentHTML("afterbegin", template(el.name));
-            console.log(inp);
+            // console.log("template=", el.name);
+            // inp.insertAdjacentHTML("afterbegin", template(el.name));
+            // console.log(inp);
+            names.push(`"name":"${el.name}"`);
           }
         });
-
-        return resp;
+        // return resp;
+        return names;
+      })
+      .then((names) => {
+        console.log("######", names);
+        inp.insertAdjacentHTML("beforeend", template(el.name));
       })
       .catch((error) => {
-        //console.log(Error.Error);
+        console.log("@@@@@@@@@@@@@@@@@@@@", Error());
       });
   }, 500)
 );
